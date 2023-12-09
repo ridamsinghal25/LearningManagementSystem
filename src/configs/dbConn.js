1e23fba92da753536ad2359e641f1a58edda6725
Ridam-Singhal <manusinghal84@gmail.com>
2023-12-09 18:01:04 +0530

    Complete writing db conn

--- src/configs/dbConn.js
+++ src/configs/dbConn.js
@@ -1,15 +1,14 @@
 import mongoose from "mongoose";
+import { DB_Name } from "../constants.js";
 
 const connectToDB = async (req, res) => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
+    const conn = await mongoose.connect(`${process.env.MONGO_URL}/${DB_Name}`);
 
     console.log(`Database connection successful!! ${conn.connection.host}`);
   } catch (error) {
-    console.log(error.message);
-    res.send({
-      success: false,
-    });
+    console.log("MONGODB CONNECTION FAILED ", error);
+    process.exit(1);
   }
 };
 
